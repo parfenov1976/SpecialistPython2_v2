49dab0a062ac29ba1ddbc081a382ef72db0bc678
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-07-24 14:00:52 +0300

    Add files via upload

--- Module2/practice/deck/04_deck_part1.py
+++ Module2/practice/deck/04_deck_part1.py
@@ -1,38 +1,61 @@
+import random
+
+
 class Card:
-    pass
-    # TODO: сюда копируем реализацию класса карты из предыдущего задания
+    HEARTS = 'Hearts'
+    DIAMONDS = 'Diamonds'
+    SPADES = 'Spades'
+    CLUBS = 'Clubs'
+
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def to_str(self):
+        suit = {'Hearts': '\u2665', 'Diamonds': '\u2666', 'Spades': '\u2660', 'Clubs': '\u2663'}
+        return f'{self.value}{suit[self.suit]}'
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
 
 
 # Задание: Теперь создадим колоду из 52-ух карт и реализуем все методы
 class Deck:
     def __init__(self):
-        # Список карт в колоде. Каждым элементом списка будет объект класса Card
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        suits = [Card.SPADES, Card.CLUBS, Card.DIAMONDS, Card.HEARTS]
         self.cards = []
+        for suit in suits:
+            for value in values:
+                card = Card(value, suit)
+                self.cards.append(card)
 
     def show(self):
-        # Принцип работы данного метода прописан в 00_task_deck.md
-        pass
+        deck_string = f'deck[{len(self.cards)}]: '
+        for card in self.cards:
+            deck_string += card.to_str() + ", "
+        return deck_string
 
     def draw(self, x):
-        # Принцип работы данного метода прописан в 00_task_deck.md
-        pass
+        drowned_cards = []
+        for _ in range(x):
+            drowned_cards.append(self.cards.pop(0))
+        return drowned_cards
 
     def shuffle(self):
-        # Обратите внимание на: https://www.w3schools.com/python/ref_random_shuffle.asp
-        pass
+        random.shuffle(self.cards)
 
 
 # Создаем колоду
 deck = Deck()
 # Выводим колоду в формате указанном в основном задании
 print(deck.show())
-# Тусуем колоду
+# Тасуем колоду
 deck.shuffle()
 print(deck.show())
-
 # Возьмем 5 карт "в руку"
 hand = deck.draw(5)
 # Выводим колоду, чтобы убедиться что 5 верхних карт отсутствуют
 print(deck.show())
 # Выводим список карт "в руке"(список hand)
-print(...)
+print(f'Hand[{len(hand)}]:', *[card.to_str() + ',' for card in hand])
