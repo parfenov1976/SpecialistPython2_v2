ac4a97d3f91ca700ffc631cd013b0c5541afb95b
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-11-05 22:17:32 +0300

    Update 01_task_TotalDict.py

--- Module4/practice/TotalDict/01_task_TotalDict.py
+++ Module4/practice/TotalDict/01_task_TotalDict.py
@@ -1,12 +1,27 @@
 # Разработать класс TotalDict со следующими возможностями:
 class TotalDict(dict):
-    pass
+    def __str__(self):
+        return f'TD:{repr(self)}'
+
+    def __add__(self, other):
+        for el in other.keys():
+            if el in self.keys():
+                self[el] += other[el]
+            else:
+                self.update([(el, other[el])])
+        return self
+
+    def most_expensive(self, ind=-1):
+        lst = [*self.items()]
+        lst.sort(key=lambda x: x[1], reverse=True)
+        return lst[0:ind]
 
 
-# 1. Объект выводит себя в консоли как обычный словарь, НО с символами TD: в начал
+# 1. Объект выводит себя в консоли как обычный словарь, НО с символами TD: в начале
 items1 = TotalDict(milk=250, bread=120, meat=450.6)
 items2 = TotalDict(juice=99.9, fish=120, milk=50.2)
 print(items1)  # TD:{'milk': 250, 'bread': 120, 'meat': 450.6}
+
 # 2. При сложении объектов TotalDict значения элементов с одинаковыми ключами суммируются
 all_items = items1 + items2
 print(all_items)  # TD:{'milk': 300.2, 'bread': 120, 'meat': 450.6, 'juice': 99.9, 'fish': 120}
